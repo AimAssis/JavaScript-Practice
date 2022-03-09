d14e5ce682a8614b38b97249f3ad3b07ac646d0b
Nikki <101257775+AimAssis@users.noreply.github.com>
2022-03-09 22:14:17 +1100

    Weather Converter

---
+++ app.js
@@ -0,0 +1,8 @@
+let Kelvin = 0
+const KelvinToCelcius = Kelvin - 273.15 
+let Celcius = 0
+const CelciusToFahrenheit = Celcius*9/5 + 32 
+
+const Fahrenheit = Kelvin * 9 / 5 + 32
+console.log("The temperature is: " + KelvinToCelcius + " Celcius");
+console.log("The temperature is: " + CelciusToFahrenheit + " Fahrenheit");
